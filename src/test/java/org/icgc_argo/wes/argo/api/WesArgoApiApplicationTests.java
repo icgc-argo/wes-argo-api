f703d647ffe01f1f02db3771940dba32b97ebbe3
Alexis Li <alexis.li@oicr.on.ca>
2020-02-11 15:41:21 -0500

    initial commit

---
+++ src/test/java/org/icgc_argo/wes/argo/api/WesArgoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.icgc_argo.wes.argo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesArgoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
