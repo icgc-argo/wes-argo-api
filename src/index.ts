74968192422b28cddc812cdcd5522710d9e76bd4
Dušan Andrić <Dusan.Andric@gmail.com>
2020-05-25 13:58:13 -0400

    Uses express middleware, passes JWT to services

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { ApolloServer } from 'apollo-server';
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
+import express, { Request } from 'express';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 const gateway = new ApolloGateway({
     serviceList: [
@@ -10,15 +14,32 @@ const gateway = new ApolloGateway({
             name: 'workflows',
             url: process.env.WORKFLOW_SEARCH_URL
         }
-    ]
+    ],
+    buildService({ name, url }) {
+        return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+                request.http.headers.set(
+                    'authorization',
+                    context.authorization
+                );
+            }
+        });
+    }
 });
 
 const server = new ApolloServer({
     gateway,
     // Disable subscriptions (not currently supported with ApolloGateway)
     subscriptions: false,
+    context: ({ req }: {req: Request}) => ({
+        authorization: req.headers?.authorization || '',
+    })
 });
 
-server.listen({port: process.env.PORT || 4000}).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-});
+server.applyMiddleware({ app });
+
+
+app.listen(port, () =>
+    console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
+);
